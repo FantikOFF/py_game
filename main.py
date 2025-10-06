8c8337b389c7956ef8ae0ca52ca010cf65a3f58b
FantikOFF <s1bnik111@gmail.com>
2025-10-06 21:44:14 +0300

    шаблон для начала взят из proglib

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pygame
 
+# инициализируем библиотеку Pygame
+pygame.init()
+
+# определяем размеры окна
+window_size = (300, 300)
+
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Ping Pong")
+clock = pygame.time.Clock()
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
